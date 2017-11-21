2ef3c2dd6232f6f54b1b3d752dd966b67d8fd326
commit <huhuixin@huhuixindeAir.lan>
2017-11-21 23:04:27 +0800

    Initial Commit

---
+++ HXDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXDB
+//
+//  Created by commet on 2017/11/21.
+//  Copyright © 2017年 commet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXDB
+//
+//  Created by commet on 2017/11/21.
+//  Copyright © 2017年 commet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
